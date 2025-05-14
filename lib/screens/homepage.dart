76e4f0fd64f334483b6bf666dd2d795ae28d6aa7
arjunatantular <arjunatantular@gmail.com>
2025-05-13 23:12:57 -0500

    Book Tracker beginning

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    myFunc();
+    return Scaffold(
+      body: Text("Placeholder")
+    );
+  }
+}
+
+void myFunc() {
+
+}
