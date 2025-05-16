5ccfcc188eca6d134994dbd709623cceffa784b4
arjunatantular <arjunatantular@gmail.com>
2025-05-16 17:37:26 -0500

    Book tracker corrupted, new folder

---
+++ book_tracker_clean/test/widget_test.dart
@@ -0,0 +1,19 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:book_tracker/script.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:book_tracker/main.dart';
+
+// Try to cover all categories of input for each (cases)
+void main() {
+
+}
